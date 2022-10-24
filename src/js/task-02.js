e5ae4bb1d72160631198e7de24e5e3d0b06270eb
Alina Romanova <alinakhraban@gmail.com>
2022-10-24 19:04:44 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+const ingredientsItems = document.createElement('li');
+ingredientsItems.classList.add('item__ingredients');
+ingredientsItems.textContent = ingredient;
+
+return ingredientsItems;
+});
+
+ingredientsList.append(...elements);
