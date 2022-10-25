d9844c32ffc2f37718e370a3d4ff515511933f96
Alina Romanova <alinakhraban@gmail.com>
2022-10-25 16:56:31 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,14 +1,20 @@
 const inputText = document.querySelector('#validation-input');
 
+
 inputText.addEventListener('blur', (event) => {
       if(event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
-       event.currentTarget.classList.add('valid')
-
+       event.currentTarget.classList.add('valid');
+       event.currentTarget.classList.remove('invalid');
+       
     } else {
-        event.currentTarget.classList.add('invalid')
+        event.currentTarget.classList.add('invalid');
+        event.currentTarget.classList.remove('valid');
+    
     };
 
 });
+
+
 
 
    
