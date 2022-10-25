1a1dc45e6b6f8e9a740aa52b046b5d62000b08f0
Alina Romanova <alinakhraban@gmail.com>
2022-10-25 23:50:42 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,39 @@
+const collectionEl = document.querySelector('#controls input');
+const addcollectionEl = document.querySelector('#boxes')
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
+console.log(collectionEl.value)
+createBtn.addEventListener("click", getAmount);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  let amount = Number(collectionEl.value);
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+ 
+   for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${width}px`;
+    box.style.height = `${height}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    addcollectionEl.append(box);
+    width += 10;
+    height += 10;
+    };   
+};
+
+function destroyBoxes() {
+  addcollectionEl.innerHTML = '';
+  collectionEl.value = '';
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
