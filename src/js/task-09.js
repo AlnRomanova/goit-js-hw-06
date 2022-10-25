95914850dec6ead3d0bb0959416965f2bd6a1292
Alina Romanova <alinakhraban@gmail.com>
2022-10-25 18:17:12 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const bodyEl = document.body;
+const btnChangeColor = document.querySelector(".change-color");
+const textColor = document.querySelector('.color')
+
+btnChangeColor.addEventListener('click', getRandomHexColor);
+
+
+function getRandomHexColor(event) {
+  const colorBodyEl = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+  if (event.currentTarget) {
+    textColor.textContent = colorBodyEl;
+  return bodyEl.style.backgroundColor = colorBodyEl; 
+  };
+
+ };
+
